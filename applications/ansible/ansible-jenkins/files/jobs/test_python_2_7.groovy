788f2f3da51f1e89b67f176967139527d437d2bd
James Carson <James.Carson.Direct@gmail.com>
2018-08-13 22:20:46 -0500

    Tests: James Carson add a job for the automated tests for Python 2.7. This is to be used as an example.

---
+++ applications/ansible/ansible-jenkins/files/jobs/test_python_2_7.groovy
@@ -0,0 +1,26 @@
+job('{{REPO}}/branches/{{BRANCH}}/test-python-2-7') {
+    scm {
+        git {
+            remote {
+                url({{JENKINS_CONFIG}})
+            }
+        branch('{{BRANCH}}')
+        }
+    }
+    triggers{
+        scm('*/15 * * * *')
+    }
+    steps{
+        shell('cd ./{{PATH}}; ./run_tests.sh')
+    }
+    publishers{
+        cobertura('**/{{PATH}}/coverage.xml'){
+            failNoReports(true)
+            sourceEncoding('ASCII')
+            methodTarget(80, 0, 0)
+            lineTarget(80, 0, 0)
+            conditionalTarget(80, 0, 0)
+        }
+        archiveJunit('**/{{PATH}}/*.xml')
+    }
+}
